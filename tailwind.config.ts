2cb144f149e36f478d84123f1220741b23ddc670
Alan Perez <goldosh7@gmail.com>
2026-05-05 20:01:21 +0000

    aqui estan los colores asi como la tipografia /* Colores */ --primary: #

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Poppins', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        body: ['Poppins', 'Inter', 'sans-serif'],
+        headline: ['Montserrat', 'Poppins', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
