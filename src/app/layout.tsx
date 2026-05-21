d712548f322e289ed119fb342d0d91e2a68f43bb
Alan Perez <goldosh7@gmail.com>
2026-05-21 04:13:02 +0000

    Abajo está la etiqueta de Google de esta cuenta. Cópiela y péguela en el

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from "@/firebase";
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'SIBF - CAI - Sistema de Asistencia',
@@ -19,6 +20,21 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&family=Montserrat:wght@300;400;500;600;700;800&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-74KCH38L32"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-74KCH38L32');
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
